d3b603459acd6a4c6bde817d26436c561a1cd7c6
21327 <rodrigo.villegaszambrano@vtsd.com>
2022-11-03 10:35:33 -0400

    yes

--- Lab05vst.java
+++ Lab05vst.java
@@ -17,6 +17,33 @@ public class Lab05vst extends Applet
         int x2 = 990;
         int y2 = 640;
         g.drawRect(10,10,width,height);
-    }
-}
+        for (int a = 0; a<50; a++){
+            g.drawLine(x1, y1, x2, y2);
+            x1 += 20;
+            y2 -= 12;
+        }
 
+        x1 = 990;
+        y1 = 640;
+        x2 = 10;
+        y2 = 640;
+        for (int a = 0; a<50; a++){
+            g.drawLine(x1, y1, x2, y2);
+            x1 -= 20;
+            y2 -= 12;
+        }
+
+        x1 = 10;
+        y1 = 640;
+        x2 = 990;
+        y2 = 640;
+        for (int a = 0; a<50; a++) {
+            g.drawLine(x1, y1, x2, y2);
+            x2 -= 20;
+            y1 -= 12;
+
+        }
+
+    }
+
+}
--- Temp.java
+++ Temp.java
@@ -1,2 +1,18 @@
-public class Temp {
+public class Temp{
+    public static void main(String args[]) {
+        //keeps track of current row
+        for (int k = 1; k < 5; k++){
+            for(int w = 0; w < k; w++){
+
+
+                System.out.println("*");
+
+            }
+
+        }
+
+    }
+
 }
+
+
